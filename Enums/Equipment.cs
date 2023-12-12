ac9ddf0819c4764b6001a0163c7e53d2598d17ef
Seb5501 <80768140+Seb5501@users.noreply.github.com>
2023-12-12 17:12:33 +0100

    Create Equipment.cs

---
+++ Enums/Equipment.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuCosmetica.Classes
+{
+    public enum Equipment
+    {
+        Espatula_Ultrasonica,
+        Punta_de_Diamante,
+        Radiofrequencia,
+        Electroporacion,
+        Alta_Frequencia,
+        Dermaplaning,
+        Dermapen,
+        Nanoder
+    }
+}
