898efcf23375d4929814d66ebd1ce0d824b2f28c
Seb5501 <80768140+Seb5501@users.noreply.github.com>
2023-12-12 17:12:49 +0100

    Add files via upload

---
+++ Enums/Locality.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuCosmetica.Classes
+{
+    public enum Locality
+    {
+        Spegazzini,
+        Tristian_Suarez,
+        Ezeiza,
+        El_Jaguel,
+        Monte_Grande,  
+        Luis_Guillon,
+        Llavallol,
+        Turdera,
+        Temperley,
+        Lomas_de_Zamora,
+        Banfield,
+        Adrogue
+    }
+}
